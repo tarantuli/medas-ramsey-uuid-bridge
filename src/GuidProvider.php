ea114d39a941595f9af54281f53adbc8c61442d7
Patrick Morpey <patrick@morphp.nl>
2023-01-23 16:56:01 +0100

    Updated packages

--- src/GuidProvider.php
+++ src/GuidProvider.php
@@ -7,6 +7,7 @@ namespace Medas\RamseyUuidBridge;
 use Medas\ServiceManager\Attributes\Service;
 use Medas\ServiceManager\Values\Interfaces\{Guid as GuidInterface, GuidProvider as GuidProviderInterface};
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidFactory;
 
 #[Service]
 class GuidProvider implements GuidProviderInterface
@@ -14,5 +15,10 @@ class GuidProvider implements GuidProviderInterface
     public function create(): GuidInterface
     {
         return new Guid(Uuid::uuid7());
+    }
+
+    public function fromBytes(string $bytes): GuidInterface
+    {
+        return new Guid((new UuidFactory())->fromBytes($bytes));
     }
 }
--- tests/Functional/GuidProviderTest.php
+++ tests/Functional/GuidProviderTest.php
@@ -13,7 +13,14 @@ class GuidProviderTest extends TestCase
     public function testProvider(): void
     {
         $guid = service(GuidProvider::class)->create();
-
         self::assertInstanceOf(Guid::class, $guid);
+    }
+
+    public function testFromBytes(): void
+    {
+        $guid = service(GuidProvider::class)->create();
+
+        $bytes = $guid->toBytes();
+        self::assertInstanceOf(Guid::class, service(GuidProvider::class)->fromBytes($bytes));
     }
 }
