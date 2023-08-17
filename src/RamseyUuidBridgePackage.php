9fc5f3691565a4f17c3ba94b5d03dd7679d088ea
Patrick Morpey <patrick@morphp.nl>
2023-08-17 09:08:23 +0200

    Updated dependencies

--- src/RamseyUuidBridgePackage.php
+++ src/RamseyUuidBridgePackage.php
@@ -13,8 +13,7 @@ class RamseyUuidBridgePackage extends BasePackage
 
     public function dependencies(): array
     {
-        return $this->dependenciesByClass([
-        ]);
+        return [];
     }
 
     public function sourceDirectory(): string
