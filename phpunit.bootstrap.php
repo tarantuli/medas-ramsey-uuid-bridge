518fb995b70b7e1b0e25120a820a67bfc9abbd4d
Patrick Morpey <patrick@morphp.nl>
2026-06-15 12:02:45 +0200

    Updated dependencies and bumped Medas packages to version 4.0

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
+use Medas\ObjectInstantiator\ObjectInstantiator;
 use Medas\RamseyUuidBridge\RamseyUuidBridgePackage;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+use Medas\ServiceManager\{ServiceConfigBuilder, ServiceManager};
 
 chdir(__DIR__);
 
-new ServiceManager(function (): ServiceConfig {
-    $config = new ServiceConfig();
+new ServiceManager(function (): ServiceConfigBuilder {
+    $config = new ServiceConfigBuilder(ObjectInstantiator::class);
 
     $config->addPackages([
         RamseyUuidBridgePackage::instance(),
